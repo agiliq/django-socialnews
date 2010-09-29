c93ee108e2f69b0f77c5509b1d4938a6c23285bc
Javed Khan <javed@agiliq.com>
2010-09-29 17:24:08 +0530

    added setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='django-socialnews',
+    description='A reddit like socialnews system for Django',
+    keywords='django, reddit',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    version="1.1",
+    author="Agiliq Solutions",
+    author_email="hello@agiliq.com",
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Internet :: WWW/HTTP :: WSGI',
+                   'Topic :: Software Development :: Libraries :: Application Frameworks',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+    url="http://www.agiliq.com/",
+    license="GPL",
+    platforms=["all"],
+)
