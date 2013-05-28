eaf368de1400b4e145765d0b3a4d5976d411a65d
krvc <krvc.it@gmail.com>
2013-05-28 16:18:41 +0530

    Modified settings/common.py

--- socialnews/settings/common.py
+++ socialnews/settings/common.py
@@ -16,18 +16,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': "",
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -60,7 +48,7 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
 
-STATIC_ROOT = "%s/static" % proj_dir
+STATIC_ROOT = "%s/../static" % proj_dir
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -68,7 +56,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "%s/static" %proj_dir,
+    "%s/../news/templates/site_media" % proj_dir,
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -152,4 +140,3 @@ AUTH_PROFILE_MODULE = 'news.UserProfile'
 PERSISTENT_SESSION_KEY = 'PERS_SESSION'
 LOGIN_REDIRECT_URL = '/recommended/'
 LOGIN_URL = '/login/'
-
